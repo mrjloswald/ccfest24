5f8d52af4529fb19c56246840a48ead1a6ddab03
mrjloswald <37389218+mrjloswald@users.noreply.github.com>
2024-01-29 10:27:30 -0500

    Update sketch.js
    Added weights and colorization based on number of flights to destination

--- 08 Preprocessing/sketch.js
+++ 08 Preprocessing/sketch.js
@@ -4,12 +4,12 @@ const colorList = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911e
 let allAirports
 let flights
 let airports // top airports
-
+let maxFlightCount = 1200 // -Infinity, find dynamically while preprocessing
+ 
 function preload() {
   allAirports = loadJSON("airportsAndFlightMappings.580x250.1.json")
 }
   
-
 function setup() {
   createCanvas(580,250)
   airports = Object.fromEntries(Object.entries(allAirports).slice( 0,15 ))
@@ -21,10 +21,13 @@ function draw() {
   for( const airport of Object.values(airports) ) {
     s = airport.square
     d = s.s/2
+    stroke('black')
     circle( airport.square.x, airport.square.y, airport.square.s )
     if( mouseX > s.x - d && mouseX < s.x + d && mouseY > s.y - d && mouseY < s.y + d ) {
       for( const dest of Object.keys(airport.outboundFlightCounts) ) {
         if( dest !== "all" && allAirports[dest] && allAirports[dest].square ) {
+          stroke(0,map(airport.outboundFlightCounts[dest], 0, maxFlightCount, 16, 256))
+          strokeWeight(map(airport.outboundFlightCounts[dest], 0, maxFlightCount, 0.5, 5))
           line(s.x, s.y, allAirports[dest].square.x, allAirports[dest].square.y)  
         }        
       }
@@ -49,7 +52,6 @@ function processData() {
   // If you want to include these places, 
   //   you have to expand your canvas size and boundary mapping
   const excludedStateCodes = ["HI","AK","PR","VI"]
-  
   allAirports = airports.getRows()
     .filter( row => excludedStateCodes.includes( row.getString("STATE") ) )
     .map( row => {
@@ -60,6 +62,9 @@ function processData() {
         const destination = row.getString("DESTINATION_AIRPORT")
         if( destination in counts ) {
           counts[destination] += 1
+          if( counts[destination] > maxFlightCount ) {
+            maxFlightCount = counts[destination]
+          }
         } else {
           counts[destination] = 0
         }
